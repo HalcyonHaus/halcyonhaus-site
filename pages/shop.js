cafe89aed11328c1676785b628620d04400fc028
HalcyonHaus <nikka.suleymanova@gmail.com>
2025-06-09 18:14:58 -0700

    Update shop.js

--- pages/shop.js
+++ pages/shop.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
-import Link from "next/link";
 import Script from "next/script";
+import Link from "next/link";
 
 export default function ShopPage() {
   return (
@@ -15,16 +15,12 @@ export default function ShopPage() {
           <Link href="/" className="hover:underline">Home</Link>
           <Link href="/about" className="hover:underline">About Me</Link>
           <Link href="/projects" className="hover:underline">Projects</Link>
-          <Link href="/shop" className="hover:underline">Shop My Home</Link>
+          <Link href="/shop" className="underline">Shop My Home</Link>
         </nav>
       </header>
 
+      {/* LTK Embed */}
       <main className="p-6">
-        <h1 className="text-center text-3xl font-light tracking-wider mb-10" style={{ fontFamily: "'Playfair Display', serif" }}>
-          SHOP MY HOME
-        </h1>
-
-        {/* LTK Widget Container */}
         <div
           id="ltkwidget-version-two669535761"
           data-appid="669535761"
@@ -41,14 +37,13 @@ export default function ShopPage() {
             data-profileid="bcf42a60-68cf-11ec-8066-7899888eea18"
           >
             <div className="rs-ltkwidget-container">
-              <div data-ui-view=""></div>
+              <div ui-view=""></div>
             </div>
           </div>
         </div>
 
-        {/* Load LTK Widget Script */}
+        {/* Load LTK Script Properly */}
         <Script
-          id="ltk-widget"
           src="https://widgets-static.rewardstyle.com/widgets2_0/client/pub/ltkwidget/ltkwidget.js"
           strategy="afterInteractive"
         />
