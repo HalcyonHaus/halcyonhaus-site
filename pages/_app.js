b444d9bdc273e2b3f8923ecd2ffd4e79811114d4
HalcyonHaus <nikka.suleymanova@gmail.com>
2025-06-10 15:15:20 -0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
-import '../styles/globals.css';
+// pages/_app.js
+import Head from "next/head";
+import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/halcyon_favicon_clean.png" type="image/png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
