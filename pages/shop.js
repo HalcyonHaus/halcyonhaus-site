ad89ea04e368848e7c086c106bb83a463cf50696
HalcyonHaus <nikka.suleymanova@gmail.com>
2025-06-09 18:29:40 -0700

    Update shop.js

--- pages/shop.js
+++ pages/shop.js
@@ -1,13 +1,25 @@
 import Head from "next/head";
+import { useEffect } from "react";
 import Link from "next/link";
 
 export default function ShopPage() {
+  useEffect(() => {
+    // Only run this in the browser
+    if (typeof window !== "undefined") {
+      const script = document.createElement("script");
+      script.src = "https://widgets-static.rewardstyle.com/widgets2_0/client/pub/ltkwidget/ltkwidget.js";
+      script.async = true;
+      document.body.appendChild(script);
+    }
+  }, []);
+
   return (
     <div className="min-h-screen bg-white text-black font-sans">
       <Head>
         <title>Shop My Home – Halcyon Haus</title>
       </Head>
 
+      {/* Navigation */}
       <header className="flex justify-end p-6 text-sm tracking-widest">
         <nav className="space-x-6 uppercase font-inter">
           <Link href="/" className="hover:underline">Home</Link>
@@ -17,19 +29,37 @@ export default function ShopPage() {
         </nav>
       </header>
 
-      <h1 className="text-[28px] md:text-[28px] font-light tracking-wider text-center mt-10" style={{ fontFamily: "'Playfair Display', serif" }}>
-        SHOP MY HOME
+      {/* Page Heading */}
+      <h1
+        className="text-center mt-12 mb-8 text-[30px] tracking-wider font-light uppercase"
+        style={{ fontFamily: "'Playfair Display', serif" }}
+      >
+        SHOPY MY HOME
       </h1>
 
-      <div className="mt-12 px-4 flex justify-center">
-        <iframe
-          src="https://widgets-static.rewardstyle.com/ltkwidget/v5.html?appid=669535761&userid=452262&profileid=bcf42a60-68cf-11ec-8066-7899888eea18&rows=4&cols=3&showframe=false&padding=4"
-          width="100%"
-          height="800"
-          style={{ border: "none", maxWidth: "100%", display: "block" }}
-          loading="lazy"
-          title="Shop My Home"
-        />
+      {/* LTK Widget Embed */}
+      <div
+        id="ltkwidget-version-two669535761"
+        data-appid="669535761"
+        className="ltkwidget-version-two px-4"
+      >
+        <div
+          widget-dashboard-settings=""
+          data-appid="669535761"
+          data-userid="452262"
+          data-rows="4"
+          data-cols="3"
+          data-showframe="false"
+          data-padding="4"
+          data-profileid="bcf42a60-68cf-11ec-8066-7899888eea18"
+        >
+          <div className="rs-ltkwidget-container">
+            <div ui-view=""></div>
+          </div>
+        </div>
+        <noscript>
+          This content requires JavaScript to be enabled. Please enable JavaScript to view.
+        </noscript>
       </div>
     </div>
   );
