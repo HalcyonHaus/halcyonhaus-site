d98952b46b211ae0587a25356052ec05664661c0
HalcyonHaus <nikka.suleymanova@gmail.com>
2025-06-10 15:14:35 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -28,7 +28,6 @@ export default function HomePage() {
     href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;700&family=Inter:wght@300;400&display=swap"
     rel="stylesheet"
   />
-  <link rel="icon" href="/halcyon_favicon_clean.png" type="image/png" />
   </Head>
 
       {/* Slideshow Background with fade */}
