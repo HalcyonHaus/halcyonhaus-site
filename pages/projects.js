697c46c2c5fa26fcfbfe810dae9bfa201ee6c79d
HalcyonHaus <nikka.suleymanova@gmail.com>
2025-06-09 20:33:25 -0700

    Update projects.js

--- pages/projects.js
+++ pages/projects.js
@@ -92,7 +92,7 @@ export default function ProjectsPage() {
         </div>
       </main>
             {/* Footer */}
-      <footer className="mt-8 pb-10 text-center text-xs text-gray-500 uppercase tracking-widest font-inter">
+      <footer className="mt-8 pb-6 text-center text-xs text-gray-500 uppercase tracking-widest font-inter">
         <p className="mb-2">© {new Date().getFullYear()} Halcyon Haus</p>
         <div className="flex justify-center space-x-6 text-gray-500">
           <a href="https://www.instagram.com/halcyonhaus_" target="_blank" rel="noopener noreferrer" aria-label="Instagram">
