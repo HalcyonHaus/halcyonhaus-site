8f95723825b7c32febb52e01d318f5265669b328
HalcyonHaus <nikka.suleymanova@gmail.com>
2025-06-10 21:47:28 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -54,13 +54,19 @@ export default function HomePage() {
 <div className="absolute top-0 right-0 z-20 p-6 text-xs tracking-widest">
   <nav className="space-x-6 uppercase font-inter text-xs">
   <Link href="/about" passHref legacyBehavior>
-    <a className="transition-opacity duration-300 hover:opacity-70">About Me</a>
+    <a className="transition-colors duration-300 text-white hover:text-neutral-300">
+  About Me
+</a>
   </Link>
   <Link href="/projects" passHref legacyBehavior>
-    <a className="transition-opacity duration-300 hover:opacity-70">Projects</a>
+    <a className="transition-colors duration-300 text-white hover:text-neutral-300">
+  Projects
+</a>
   </Link>
   <Link href="/shop" passHref legacyBehavior>
-    <a className="transition-opacity duration-300 hover:opacity-70">Shop My Home</a>
+    <a className="transition-colors duration-300 text-white hover:text-neutral-300">
+  Shop My Home
+</a>
   </Link>
 </nav>
 </div>
