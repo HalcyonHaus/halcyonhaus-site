1c340ede623def7a70b95d933b0c6bd42bcea124
HalcyonHaus <nikka.suleymanova@gmail.com>
2025-06-10 18:02:41 -0700

    Update ProjectCarousel.js

--- components/ProjectCarousel.js
+++ components/ProjectCarousel.js
@@ -6,31 +6,31 @@ export default function ProjectCarousel({ title, images }) {
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const goToPrev = () => {
-    if (currentIndex > 0) {
-      setCurrentIndex(currentIndex - 1);
-    }
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? images.length - 1 : prevIndex - 1
+    );
   };
 
   const goToNext = () => {
-    if (currentIndex < images.length - 1) {
-      setCurrentIndex(currentIndex + 1);
-    }
+    setCurrentIndex((prevIndex) =>
+      prevIndex === images.length - 1 ? 0 : prevIndex + 1
+    );
   };
 
   return (
-    <div className="group relative w-full aspect-[4/4.3] md:aspect-[4/4.9] overflow-hidden">
-      <Image
-        src={images[currentIndex]}
-        alt={title}
-        fill
-        style={{ objectFit: "cover" }}
-        className="transition-transform duration-300"
-      />
+    <div>
+      <div className="group relative w-full aspect-[4/4.3] md:aspect-[4/4.9] overflow-hidden rounded-md">
+        <Image
+          src={images[currentIndex]}
+          alt={title}
+          fill
+          style={{ objectFit: "cover" }}
+          className="transition-transform duration-300"
+        />
 
-      {/* Arrows */}
-      {images.length > 1 && (
-        <>
-          {currentIndex > 0 && (
+        {/* Arrows */}
+        {images.length > 1 && (
+          <>
             <button
               onClick={goToPrev}
               className="absolute left-3 top-1/2 -translate-y-1/2 z-20 opacity-0 group-hover:opacity-100 transition-opacity duration-300"
@@ -42,8 +42,6 @@ export default function ProjectCarousel({ title, images }) {
                 className="text-white/90 hover:text-white"
               />
             </button>
-          )}
-          {currentIndex < images.length - 1 && (
             <button
               onClick={goToNext}
               className="absolute right-3 top-1/2 -translate-y-1/2 z-20 opacity-0 group-hover:opacity-100 transition-opacity duration-300"
@@ -55,23 +53,23 @@ export default function ProjectCarousel({ title, images }) {
                 className="text-white/90 hover:text-white"
               />
             </button>
-          )}
-        </>
-      )}
+          </>
+        )}
 
-      {/* Dots */}
-      <div className="absolute bottom-2 w-full flex justify-center space-x-1 z-20">
-        {images.map((_, idx) => (
-          <div
-            key={idx}
-            className={`h-1.5 w-1.5 rounded-full ${
-              idx === currentIndex ? "bg-white" : "bg-white/50"
-            }`}
-          />
-        ))}
+        {/* Dots */}
+        <div className="absolute bottom-2 w-full flex justify-center space-x-1 z-20">
+          {images.map((_, idx) => (
+            <div
+              key={idx}
+              className={`h-1.5 w-1.5 rounded-full ${
+                idx === currentIndex ? "bg-white" : "bg-white/50"
+              }`}
+            />
+          ))}
+        </div>
       </div>
 
-      {/* Title */}
+      {/* Title Below Image */}
       <p className="font-inter uppercase tracking-widest text-sm mt-4 text-center pt-4">
         {title}
       </p>
