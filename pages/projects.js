a6143737e26e5523d5be1a530801637b81b03875
HalcyonHaus <nikka.suleymanova@gmail.com>
2025-06-10 20:26:38 -0700

    Update projects.js

--- pages/projects.js
+++ pages/projects.js
@@ -16,9 +16,9 @@ const projects = [
     title: "Halcyon Haus",
     images: [
       "/images/DSC01677.jpeg",
-      "/images/IMG_4599.jpeg"
+      "/images/IMG_4599.jpeg",
       "/images/DSC01631.jpeg",
-      "/images/DSC01662.jpeg"
+      "/images/DSC01662.jpeg",
       "/images/DC7801C0-7521-4A4A-86F3-228C9F155D6E.jpeg",
       "/images/53757896-768E-436A-A242-E1EB1C839F05.jpeg",
       "/images/DSC01503.jpeg",
