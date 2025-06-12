0340095fabcb435b47209ffe3112f82a02993f32
HalcyonHaus <nikka.suleymanova@gmail.com>
2025-06-11 21:11:23 -0700

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -4,8 +4,10 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <link rel="icon" type="image/png" href="/favicon-32x32.png" />
-      </Head>
+  <link rel="icon" href="/favicon-32x32.png" sizes="32x32" />
+  <link rel="icon" href="/favicon-16x16.png" sizes="16x16" />
+  <link rel="apple-touch-icon" href="/favicon-180x180.png" sizes="180x180" />
+</Head>
       <body>
         <Main />
         <NextScript />
