7323f40030b98d8fd64feec09d84e52b65195486
HalcyonHaus <nikka.suleymanova@gmail.com>
2025-06-11 21:01:26 -0700

    Create _document.js

---
+++ pages/_document.js
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link rel="icon" type="image/png" href="/favicon-32x32.png" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
