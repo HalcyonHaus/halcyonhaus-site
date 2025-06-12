d3a2ac9524545110b5db7db26fea34956c2d919a
HalcyonHaus <nikka.suleymanova@gmail.com>
2025-06-11 20:48:51 -0700

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -61,7 +61,7 @@ export default function AboutPage() {
 </section>
 
       {/* Footer */}
-      <footer className="mt-6 pb-6 text-center text-xs text-gray-500 uppercase tracking-widest font-inter">
+      <footer className="mt-10 pb-10 text-center text-xs text-gray-500 uppercase tracking-widest font-inter">
         <p className="mb-2">© {new Date().getFullYear()} Halcyon Haus</p>
         <div className="flex justify-center space-x-6 text-gray-500">
           <a href="https://www.instagram.com/halcyonhaus_" target="_blank" rel="noopener noreferrer" aria-label="Instagram">
