b9bf6fc57ca401b069a7d241427d1d1eb6008e08
HalcyonHaus <nikka.suleymanova@gmail.com>
2025-06-13 22:45:39 -0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,21 @@
 // pages/_app.js
 import Head from "next/head";
 import "../styles/globals.css";
+import ChatBubble from "../components/ChatBubble";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      {/* You can keep any global <Head> tags here if you like */}
+      <Head>
+        {/* e.g. your site-wide meta tags, favicons, etc. */}
+      </Head>
+
+      {/* Render the active page */}
       <Component {...pageProps} />
+
+      {/* Floating chat bubble on every page */}
+      <ChatBubble />
     </>
   );
 }
