5ea218947991b386910c229487e6d6467e29bdcc
HalcyonHaus <nikka.suleymanova@gmail.com>
2025-06-13 22:20:13 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,19 @@
- import Head from "next/head";
--import DesignBotChat from "../components/DesignBotChat";
-+import ChatBubble from "../components/ChatBubble";
+import Head from "next/head";
+import ChatBubble from "../components/ChatBubble";
 
- export default function Home() {
-   return (
-     <>
-       <Head>
-         <title>Halcyon Haus</title>
-       </Head>
-       <main className="min-h-screen">
-         {/* Your existing content here… */}
--
--        {/* Inline chat for testing */}
--        {/* <section className="w-full max-w-md mx-auto p-8">
--          <h2 className="text-2xl font-semibold mb-4">Chat with Halcyon Haus</h2>
--          <DesignBotChat />
--        </section> */}
-+        {/* Your page’s real content… */}
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Halcyon Haus</title>
+      </Head>
 
-+        {/* Floating chat bubble */}
-+        <ChatBubble />
-       </main>
-     </>
-   );
- }
+      <main className="min-h-screen bg-[var(--bg)]">
+        {/* Your existing homepage sections go here… */}
+
+        {/* Floating chat bubble */}
+        <ChatBubble />
+      </main>
+    </>
+  );
+}
