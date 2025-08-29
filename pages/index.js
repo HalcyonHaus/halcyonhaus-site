c93f7259b5aca771a491d77cbfc4344600b7f2b1
HalcyonHaus <nikka.suleymanova@gmail.com>
2025-08-28 18:18:15 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ import { useEffect, useState } from "react";
 const images = [
   "/images/HERO1.JPG",
   "/images/HERO2.JPG",
-  "/images/DINING2.png",
+  "/images/DINING2.JPG",
   "/images/PRIMARYBATH00.JPG.png",
 ];
 
