fc20c7976b613a4a1a323895d1aa3cfb4ce75d9a
HalcyonHaus <nikka.suleymanova@gmail.com>
2025-08-30 11:48:49 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -86,7 +86,7 @@ export default function HomePage() {
               Welcome to Halcyon Haus — an interior design studio founded by Nikka Winchell, rooted in thoughtful, transitional design.
             </p>
             <p>
-              At Halcyon Haus, I guide you through a warm, hands-on design journey rooted in transitional style, where tradition meets modern simplicity. With an eye for curated strategy and balanced composition, every project is tailored to your lifestyle and vision.
+              At Halcyon Haus, I guide you through a hands-on design journey rooted in transitional style, where tradition meets modern simplicity. With a curated strategy and balanced composition, every project is tailored to your lifestyle and vision.
             </p>
           </div>
 
