231441fde681e2e2f691a9fc38a098a1f9efb548
HalcyonHaus <nikka.suleymanova@gmail.com>
2025-09-02 15:56:35 -0600

    Update canyon-cottage-kitchen.js

--- pages/projects/canyon-cottage-kitchen.js
+++ pages/projects/canyon-cottage-kitchen.js
@@ -71,7 +71,7 @@ export default function CanyonCottageKitchen() {
         <div className="grid grid-cols-1 md:grid-cols-12 gap-12 items-center mb-16 -ml-5 md:-ml-10 w-[95%] md:w-[110%]">
           <div className="md:col-span-6">
             <img
-              src="/images/KITCHENBLOG2.JPG"
+              src="/images/KITCHENBLOG3.JPG"
               alt="Process or Detail Shot"
               className="w-full rounded-md object-cover"
             />
@@ -85,7 +85,7 @@ export default function CanyonCottageKitchen() {
 
         {/* Final two-up layout */}
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mb-16 -ml-5 md:-ml-10 w-[95%] md:w-[110%]">
-          <img src="/images/KITCHENBLOG3.JPG" alt="Detail 3" className="w-full rounded-md object-cover" />
+          <img src="/images/KITCHENBLOG2.JPG" alt="Detail 3" className="w-full rounded-md object-cover" />
           <img src="/images/KITCHENBLOG4.JPG" alt="Detail 4" className="w-full rounded-md object-cover" />
         </div>
 
