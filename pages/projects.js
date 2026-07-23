74fe4159c379496365d942f88d43127ff8b38745
HalcyonHaus <nikka.suleymanova@gmail.com>
2026-07-22 17:15:50 -0700

    Update projects.js

--- pages/projects.js
+++ pages/projects.js
@@ -28,6 +28,7 @@ const projects = [
   },
   {
     title: "Canyon Cottage Primary Bath",
+        slug: "cottage-canyon-primary-bath",
     images: [
       "/images/PRIMARYBATH00.JPG",
       "/images/PRIMARYBATH2.JPG",
